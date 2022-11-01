283dc616f2bd971b436589834d2ec567007fda6e
afsana1996sultana <afsana1996sultana@gmail.com>
2022-11-01 15:29:14 +0600

    few change

--- resources/views/pages/frontend/contact/index.blade.php
+++ resources/views/pages/frontend/contact/index.blade.php
@@ -113,7 +113,7 @@
     <div class="map-outer">
         <div class="no-padding">
             <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.4034730697995!2d90.39332511637642!3d23.875307447688005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c539b2e97ced%3A0x9c18c0c64bae9ec4!2sCross-Link%20Engineering%20Ltd!5e0!3m2!1sen!2sbd!4v1663753946739!5m2!1sen!2sbd" width="1340" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3648.4034730697995!2d90.39332511637642!3d23.875307447688005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c539b2e97ced%3A0x9c18c0c64bae9ec4!2sCross-Link%20Engineering%20Ltd!5e0!3m2!1sen!2sbd!4v1663753946739!5m2!1sen!2sbd" width="1340" height="500" style="border:0; min-width: 100%;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
     </div>
